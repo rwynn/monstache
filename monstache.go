019add46613c8494c3d28acd7d0b55f80ab8c17c
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-02-17 17:04:49 -0500

    bump version higher

--- monstache.go
+++ monstache.go
@@ -54,7 +54,7 @@ var patchNamespaces map[string]bool
 var chunksRegex = regexp.MustCompile("\\.chunks$")
 var systemsRegex = regexp.MustCompile("system\\..+$")
 
-const version = "3.6.6"
+const version = "3.7.0"
 const mongoURLDefault string = "localhost"
 const resumeNameDefault string = "default"
 const elasticMaxConnsDefault int = 10
