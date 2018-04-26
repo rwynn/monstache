4dfce9042dcd267de94c199ee123f62049e4d88f
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-04-26 17:27:38 -0400

    bump version for bugfix release of gtm library

--- monstache.go
+++ monstache.go
@@ -58,7 +58,7 @@ var routingNamespaces map[string]bool = make(map[string]bool)
 var chunksRegex = regexp.MustCompile("\\.chunks$")
 var systemsRegex = regexp.MustCompile("system\\..+$")
 
-const version = "3.13.0"
+const version = "3.13.1"
 const mongoURLDefault string = "localhost"
 const resumeNameDefault string = "default"
 const elasticMaxConnsDefault int = 10
