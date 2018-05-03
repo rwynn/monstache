28dec5444ff33a759edc9404b8c37ac87570a7e0
Ahmad Magdy <magdy.a@hotmail.com>
2018-05-03 13:18:49 +0200

    Add some fixes and comments to docker files

--- create-build-in-docker.sh
+++ create-build-in-docker.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# # Build a docker image
 # docker build . -f Dockerfile-build -t monstache-build
 docker build . -f Dockerfile-build-from-scratch -t monstache-build
 
+# Start a container from the newly built docker image
 docker run --rm -d monstache-build tail -f /go/src/app/monstache.go
 
+# Get the container id of the last created container
 CONTAINER_ID=$(docker ps -l -q)
 
+# If the folder docker-build exists locally
 if [ -d docker-build ] ; then
+ # Then remove it
  rm -r docker-build
 fi
 
+# Copy the build folder from the last created container to the folder docker-build locally
 docker cp "$CONTAINER_ID":/go/src/app/build ./docker-build
 # docker cp "$CONTAINER_ID":/tmp/build ./docker-build
 
+# Stop the container (it'll be removed automatically once stopped, as we used `--rm`)
 docker stop "$CONTAINER_ID"
--- docker/builder-base/README.md
+++ docker/builder-base/README.md
@@ -2,6 +2,8 @@
 
 This image will be used as a golang base image to build the tool
 
+It installs required packages to install dependencies and build the app, creating it and pushing it to docker hub, will save the time it takes to build this image on every machine
+
 It uses `golang:alpine` as a base and installs the following packages:
 
 `go`
