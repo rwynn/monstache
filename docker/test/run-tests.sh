d37d56a1118e3d2f68f52a65e6c9c183e770f47d
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-05-17 16:17:00 -0400

    integrate with travis

--- docker/test/run-tests.sh
+++ docker/test/run-tests.sh
@@ -2,4 +2,4 @@
 
 export COMPOSE_FILE=docker-compose.test.yml
 
-docker-compose down --remove-orphans ; docker-compose up --force-recreate --build --abort-on-container-exit
+docker-compose down --remove-orphans ; docker-compose up --force-recreate --build --abort-on-container-exit --exit-code-from sut
