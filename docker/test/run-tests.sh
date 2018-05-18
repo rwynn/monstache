8f590ee4f4ba18af842a65c39aec84506b2a38f2
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-05-18 16:42:22 -0400

    Merge PR #70 to master

--- docker/test/run-tests.sh
+++ docker/test/run-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 export COMPOSE_FILE=docker-compose.test.yml
+export COMPOSE_PROJECT_NAME=monstache
+
+# The network created by docker-compose will be called ${COMPOSE_PROJECT_NAME}_test as we have the network test in docker-compose
 
 docker-compose down --remove-orphans ; docker-compose up --force-recreate --build --abort-on-container-exit --exit-code-from sut
