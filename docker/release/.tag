754fd48d88fd08c5cffca5a5557220ca642b2974
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-08-02 21:39:15 -0400

    Monstache v4.9.0
    This release includes a small fix to omit version information on deletes
    when the index-as-update setting is ON (to match the omitted version
    information at indexing time).  This release also addresses issue #89 by
    making the oplog field names configurable.

--- docker/release/.tag
+++ docker/release/.tag
@@ -1,1 +1,1 @@
-rwynn/monstache:4.8.0
+rwynn/monstache:4.9.0
--- monstache.go
+++ monstache.go
@@ -61,7 +61,7 @@ var routingNamespaces map[string]bool = make(map[string]bool)
 var chunksRegex = regexp.MustCompile("\\.chunks$")
 var systemsRegex = regexp.MustCompile("system\\..+$")
 
-const version = "4.8.0"
+const version = "4.9.0"
 const mongoURLDefault string = "localhost"
 const resumeNameDefault string = "default"
 const elasticMaxConnsDefault int = 4
