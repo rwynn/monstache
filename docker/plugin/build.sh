1350f9c41cb4b6876fdae8e72c1554e7d61d4d5e
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-09-28 15:24:26 -0400

    fix plugin build to use depends from releases

--- docker/plugin/build.sh
+++ docker/plugin/build.sh
@@ -3,24 +3,17 @@
 plugin=$(<.plugin)
 
 # # Build a docker image
-docker build \
-. \
--f ./Dockerfile \
---build-arg PLUGIN="$plugin" \
--t monstache-plugin
+docker build --build-arg PLUGIN="$plugin" \
+       -f ./Dockerfile -t monstache-plugin .
 
-# Start a container from the newly built docker image (the `tail -f` here is just a dummy process to keep the container running until stopped -tail with follow any file-)
-docker run \
---rm \
--d \
-monstache-plugin \
-tail -f /proc/loadavg
+# Start a container from the newly built docker image
+docker run --rm -d monstache-plugin tail -f /go/src/cache-app/$plugin.go
 
 # Get the container id of the last created container
 CONTAINER_ID=$(docker ps -l -q)
 
+
 # If the folder docker-build exists locally
-# TODO add a step to confirm from the user before removing the existing folder
 if [ -d docker-build ] ; then
  # Then remove it
  rm -r docker-build
@@ -28,7 +21,7 @@ fi
 
 mkdir docker-build
 
-docker cp "$CONTAINER_ID":/go/src/app/$plugin.so ./docker-build/$plugin.so
+docker cp "$CONTAINER_ID":/go/src/cache-app/$plugin.so ./docker-build/$plugin.so
 
 # Stop the container (it'll be removed automatically once stopped, as we used `--rm`)
 docker stop "$CONTAINER_ID"
