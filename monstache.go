13b0554003fb7559987bd33161b6b2645932d2a5
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-10-15 23:00:09 -0400

    pass by value to get a copy of the config

--- monstache.go
+++ monstache.go
@@ -1824,7 +1824,7 @@ func (config *configOptions) loadGridFsConfig() *configOptions {
 	return config
 }
 
-func (config *configOptions) dump() {
+func (config configOptions) dump() {
 	if config.MongoURL != "" {
 		config.MongoURL = cleanMongoURL(config.MongoURL)
 	}
