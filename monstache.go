600923802b328869401f45f5165cda2ba94de536
Ryan Wynn <ryan.m.wynn@gmail.com>
2018-11-07 07:09:29 -0500

    fix for issue #123

--- monstache.go
+++ monstache.go
@@ -1700,6 +1700,7 @@ func (config *configOptions) loadConfigFile() *configOptions {
 		if config.IndexFiles {
 			if len(config.FileNamespaces) == 0 {
 				config.FileNamespaces = tomlConfig.FileNamespaces
+				config.loadGridFsConfig()
 			}
 		}
 		if config.Worker == "" {
@@ -1714,8 +1715,8 @@ func (config *configOptions) loadConfigFile() *configOptions {
 		if config.EnablePatches {
 			if len(config.PatchNamespaces) == 0 {
 				config.PatchNamespaces = tomlConfig.PatchNamespaces
+				config.loadPatchNamespaces()
 			}
-			config.loadPatchNamespaces()
 		}
 		if len(config.RoutingNamespaces) == 0 {
 			config.RoutingNamespaces = tomlConfig.RoutingNamespaces
@@ -2433,19 +2434,27 @@ func routeOp(config *configOptions, mongo *mgo.Session, bulk *elastic.BulkProces
 					}
 				}
 				skip = allSkip
-				rdata := make(map[string]interface{})
-				for k, v := range op.Data {
-					rdata[k] = v
+				if skip {
+					out.relateC <- op
+				} else {
+					rop := &gtm.Op{
+						Id:        op.Id,
+						Operation: op.Operation,
+						Namespace: op.Namespace,
+						Source:    op.Source,
+						Timestamp: op.Timestamp,
+					}
+					var data []byte
+					data, err = bson.Marshal(op.Data)
+					if err == nil {
+						var m map[string]interface{}
+						err = bson.Unmarshal(data, &m)
+						if err == nil {
+							rop.Data = m
+						}
+					}
+					out.relateC <- rop
 				}
-				rop := &gtm.Op{
-					Id:        op.Id,
-					Operation: op.Operation,
-					Namespace: op.Namespace,
-					Source:    op.Source,
-					Timestamp: op.Timestamp,
-					Data:      rdata,
-				}
-				out.relateC <- rop
 			}
 		}
 		if !skip {
