ae9358c140cad4ccfa93be1691e03321c009f0ce
Ryan Wynn <ryan.m.wynn@gmail.com>
2019-05-06 23:28:50 +0000

    fix formatting and remove dead code

--- monstachemap/encoding.go
+++ monstachemap/encoding.go
@@ -1,8 +1,8 @@
 package monstachemap
 
 import (
-    "time"
-    "errors"
+	"errors"
+	"time"
 )
 
 const timeJsonFormat = "2006-01-02T15:04:05.000Z07:00"
@@ -22,17 +22,8 @@ func (t Time) MarshalJSON() ([]byte, error) {
 	return b, nil
 }
 
-/*func (tw *Time) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		return nil
-	}
-	t, err := time.Parse(`"`+timeJsonFormat+`"`, string(data))
-	*tw = mtime{t}
-	return err
-}*/
-
 func ConvertSliceForJSON(a []interface{}) []interface{} {
-    var avs []interface{}
+	var avs []interface{}
 	for _, av := range a {
 		var avc interface{}
 		switch achild := av.(type) {
@@ -40,7 +31,7 @@ func ConvertSliceForJSON(a []interface{}) []interface{} {
 			avc = ConvertMapForJSON(achild)
 		case []interface{}:
 			avc = ConvertSliceForJSON(achild)
-        case time.Time:
+		case time.Time:
 			avc = Time{achild}
 		default:
 			avc = av
@@ -58,8 +49,8 @@ func ConvertMapForJSON(m map[string]interface{}) map[string]interface{} {
 			o[k] = ConvertMapForJSON(child)
 		case []interface{}:
 			o[k] = ConvertSliceForJSON(child)
-        case time.Time:
-            o[k] = Time{child}
+		case time.Time:
+			o[k] = Time{child}
 		default:
 			o[k] = v
 		}
