3915f33d4a89f92569b12915e20d967af499a401
Ryan Wynn <ryan.m.wynn@gmail.com>
2019-05-21 19:52:33 +0000

    prevent replay of last processed event

--- monstache.go
+++ monstache.go
@@ -3751,6 +3751,7 @@ func main() {
 				if err = result.Decode(&doc); err == nil {
 					if doc["ts"] != nil {
 						ts = doc["ts"].(primitive.Timestamp)
+                        ts.I += 1
 					}
 				}
 			}
