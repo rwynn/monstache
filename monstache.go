8e0aa74f706c0ddc201771d3b8f0fe7eae7cb4ce
kindyakov <kindyakov@kolesa.kz>
2019-09-26 16:25:12 +0600

    Try ti add Last Time stamp into instance Status response

--- monstache.go
+++ monstache.go
@@ -219,11 +219,12 @@ type httpServerCtx struct {
 }
 
 type instanceStatus struct {
-	Enabled     bool
-	Pid         int
-	Hostname    string
-	ClusterName string
-	ResumeName  string
+	Enabled     bool   `json:"enabled"`
+	Pid         int    `json:"pid"`
+	Hostname    string `json:"hostname"`
+	ClusterName string `json:"cluster"`
+	ResumeName  string `json:"resumeName"`
+	LastTs      int64  `json:"lastTs"`
 }
 
 type configOptions struct {
@@ -3583,7 +3584,7 @@ func (ctx *httpServerCtx) serveHttp() {
 	}
 }
 
-func (ctx *httpServerCtx) buildServer() {
+func (ctx *httpServerCtx) buildServer(lts *int64) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/started", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
@@ -3606,6 +3607,7 @@ func (ctx *httpServerCtx) buildServer() {
 			Hostname:    hostname,
 			ResumeName:  ctx.config.ResumeName,
 			ClusterName: ctx.config.ClusterName,
+			LastTs:      *lts,
 		}
 		data, err := json.Marshal(status)
 		if err != nil {
@@ -3803,6 +3805,7 @@ func saveTimestampFromReplStatus(session *mgo.Session, config *configOptions) {
 }
 
 func main() {
+	var lts int64
 	enabled := true
 	defer handlePanic()
 	config := &configOptions{
@@ -3881,7 +3884,7 @@ func main() {
 			config:  config,
 			enabled: &enabled,
 		}
-		hsc.buildServer()
+		hsc.buildServer(&lts)
 		go hsc.serveHttp()
 	}
 	go func() {
@@ -4182,6 +4185,7 @@ func main() {
 				bulk.Flush()
 				if saveTimestamp(mongo, lastTimestamp, config); err == nil {
 					lastSavedTimestamp = lastTimestamp
+					tsch <- lastSavedTimestamp.Time().Unix()
 				} else {
 					processErr(err, config)
 				}
