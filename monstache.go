d6b576fcbb14882922cf8f8aa6a786adac4969da
Ryan Wynn <ryan.m.wynn@gmail.com>
2020-02-22 07:14:17 -0500

    need to propogate dot-notation setting

--- monstache.go
+++ monstache.go
@@ -1663,6 +1663,7 @@ func (config *configOptions) loadReplacements() {
 					SrcField:      r.SrcField,
 					MatchField:    r.MatchField,
 					KeepSrc:       r.KeepSrc,
+					DotNotation:   r.DotNotation,
 					MaxDepth:      r.MaxDepth,
 					db:            database,
 					col:           collection,
