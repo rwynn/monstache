ae0100affebcc679339c385b4527a81d59b233a0
Ryan Wynn <ryan.m.wynn@gmail.com>
2020-04-09 11:02:02 -0400

    bump version

--- docker/release/.tag
+++ docker/release/.tag
@@ -1,1 +1,1 @@
-rwynn/monstache:6.5.2
+rwynn/monstache:6.5.3
--- monstache.go
+++ monstache.go
@@ -80,7 +80,7 @@ var chunksRegex = regexp.MustCompile("\\.chunks$")
 var systemsRegex = regexp.MustCompile("system\\..+$")
 var exitStatus = 0
 
-const version = "6.5.2"
+const version = "6.5.3"
 const mongoURLDefault string = "mongodb://localhost:27017"
 const resumeNameDefault string = "default"
 const elasticMaxConnsDefault int = 4
