6cdd72cb08ed45c2f8a422610ce52e803290907f
Ryan Wynn <ryan.m.wynn@gmail.com>
2022-10-14 13:10:42 +0000

    New version

--- monstache.go
+++ monstache.go
@@ -82,7 +82,7 @@ var chunksRegex = regexp.MustCompile("\\.chunks$")
 var systemsRegex = regexp.MustCompile("system\\..+$")
 var exitStatus = 0
 
-const version = "6.7.10"
+const version = "6.7.11"
 const mongoURLDefault string = "mongodb://localhost:27017"
 const resumeNameDefault string = "default"
 const elasticMaxConnsDefault int = 4
