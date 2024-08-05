befac1660076147336022c4330b136687e2d41db
lsidoree <isidore.mikorey-nilsson@pytheascapital.com>
2024-08-05 12:03:53 +0200

    feat: add conf and mapping for stream sync of relationships in elasticsearch

---
+++ formatElasticRelations.js
@@ -0,0 +1,95 @@
+module.exports = function (doc) {
+  if (
+    !doc.clientSettings ||
+    !doc.clientSettings.supplierClientId ||
+    !doc.clientSettings.customerClientId
+  ) {
+    var clientSettings = pipe(
+      [
+        {
+          $project: {
+            supplierClientId: {
+              $cond: [
+                {
+                  $in: [
+                    doc.supplier.companyId,
+                    { $setUnion: ["$subsidiaryIds", ["$companyId"]] },
+                  ],
+                },
+                "$clientId",
+                "$$REMOVE",
+              ],
+            },
+            customerClientId: {
+              $cond: [
+                {
+                  $in: [
+                    doc.customer.companyId,
+                    { $setUnion: ["$subsidiaryIds", ["$companyId"]] },
+                  ],
+                },
+                "$clientId",
+                "$$REMOVE",
+              ],
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            customerClientIds: {
+              $addToSet: "$customerClientId",
+            },
+            supplierClientIds: {
+              $addToSet: "$supplierClientId",
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            supplierClientId: { $first: "$supplierClientIds" },
+            customerClientId: { $first: "$customerClientIds" },
+          },
+        },
+      ],
+      { collection: "clientSettings" }
+    )[0];
+
+    doc.supplierClientId = clientSettings.supplierClientId;
+    doc.customerClientId = clientSettings.customerClientId;
+  }
+
+  if (doc.supplier || doc.customer) {
+    if (doc.supplier && doc.supplier.companyName) {
+      doc.supplierName = doc.supplier.companyName;
+    }
+    if (doc.customer && doc.customer.companyName) {
+      doc.customerName = doc.customer.companyName;
+    }
+
+    var addressFields = ["street", "zipCode", "city", "country", "countryCode"];
+    for (var i = 0; i < addressFields.length; i++) {
+      if (
+        doc.supplier &&
+        doc.supplier.address &&
+        doc.supplier.address[addressFields[i]]
+      ) {
+        doc["supplierAddress_" + addressFields[i]] =
+          doc.supplier.address[addressFields[i]];
+      }
+      if (
+        doc.customer &&
+        doc.customer.address &&
+        doc.customer.address[addressFields[i]]
+      ) {
+        doc["customerAddress_" + addressFields[i]] =
+          doc.customer.address[addressFields[i]];
+      }
+    }
+    delete doc.supplier;
+    delete doc.customer;
+  }
+
+  return doc;
+};
